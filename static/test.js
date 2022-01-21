b28db215702a9dd09ba0b0337fb8a62a635e0b07
developython14 <djmel1955@gmail.com>
2022-01-22 00:38:15 +0600

    implementing flask download file from server

--- static/test.js
+++ static/test.js
@@ -11,35 +11,3 @@ function postData() {
 
 
 
-  function download() {
-    var  container = document.querySelector('.contaner') ;
-    container.style.color = 'red'
-}
-
-
-// Example POST method implementation:
-function postData() {
-    window.location = "/download";
-  }
-
-  function download() {
-    var  container = document.querySelector('.contaner') ;
-    container.style.color = 'red'
-}
-
-
-// Example POST method implementation:
-function postData() {
-    window.location = "/download";
-  }
-
-  function download() {
-    var  container = document.querySelector('.contaner') ;
-    container.style.color = 'red'
-}
-
-
-// Example POST method implementation:
-function postData() {
-    window.location = "/download";
-  }
