f982a54602f0da0c9ff5051f3795a1623ca82399
developython14 <djmel1955@gmail.com>
2022-01-22 06:30:16 +0600

    learn jwt

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from distutils.log import debug
-from flask import Flask , render_template ,url_for
+from flask import Flask, jsonify , render_template, request ,url_for
 from flask import send_file, send_from_directory, safe_join, abort
 import os 
+from flask.views import MethodView
+
 
 
 
@@ -9,6 +11,17 @@ app = Flask(__name__)
 main_path = os.path.dirname(os.path.abspath(__file__)) 
 app.config["down"] =os.path.join(main_path,'images')  
 
+
+
+
+class toma(MethodView) : 
+    def get(self):
+        return 'mustapha belkassem'
+    def post (self) :
+        return jsonify([1,2,3])
+
+view = toma.as_view('mustapha')
+app.add_url_rule('/alger' , view_func= view ,methods=['GET','POST'])
 
 @app.route("/")
 def hello_world():
@@ -18,6 +31,13 @@ def hello_world():
 @app.route('/download')
 def down():
     return send_from_directory(app.config["down"],path =app.config["down"], filename='zaki12.jpg', as_attachment=True)
+
+
+@app.route('/ytb')
+def yout() : 
+    auth = request.authorization
+    return 'algerien'
+
 
 if __name__ == '__main__':
     app.run( debug=True)
